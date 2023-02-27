3db31759d772b7401b00012ff0ccc3d357e6a01d
Yair Cid Cerón <Yair.Ceron.-ND@disney.com>
2023-02-27 17:28:05 -0600

    Initial Commit

---
+++ YayiStore/YayiStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  YayiStoreApp.swift
+//  YayiStore
+//
+//  Created by Yair Cid on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct YayiStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
